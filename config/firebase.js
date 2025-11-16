08758203bf4ebeff4bee1ad19bc73f2d39755471
Kiran Gangurde <89308541+kiran3311@users.noreply.github.com>
2025-11-16 16:11:06 +0530

    Update Firebase service account configuration
    Uncomment service account import and comment out env variable usage.

--- config/firebase.js
+++ config/firebase.js
@@ -1,9 +1,9 @@
 const admin = require("firebase-admin");
 
-//const serviceAccount = require("../serviceAccountKey.json"); 
+const serviceAccount = require("../serviceAccountKey.json"); 
 // OR use env variables in production
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+//const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
