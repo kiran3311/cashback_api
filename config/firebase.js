6e837af3f5963bd44ac4810a1908ad2a9cb08120
Kiran Gangurde <89308541+kiran3311@users.noreply.github.com>
2025-11-23 19:38:18 +0530

    Refactor Firebase initialization with environment variables

--- config/firebase.js
+++ config/firebase.js
@@ -1,13 +1,19 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../serviceAccountKey.json"); 
+//const serviceAccount = require("../serviceAccountKey.json"); 
 // OR use env variables in production
 
 //const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+
+if (!admin.apps.length) {
+  const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+}
+
 
 
 // admin.initializeApp({
