b582b3843c6a5e8cc86c2402c0f2ac848bc33e7a
Kiran Gangurde <89308541+kiran3311@users.noreply.github.com>
2025-11-23 19:04:20 +0530

    Update Firebase initialization with service account

--- config/firebase.js
+++ config/firebase.js
@@ -1,23 +1,23 @@
 const admin = require("firebase-admin");
 
-//const serviceAccount = require("../../serviceAccountKey.json"); 
+const serviceAccount = require("../serviceAccountKey.json"); 
 // OR use env variables in production
 
 //const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-// });
-
-
 admin.initializeApp({
-    credential: admin.credential.cert({
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    })
-    
+  credential: admin.credential.cert(serviceAccount),
 });
+
+
+// admin.initializeApp({
+//     credential: admin.credential.cert({
+//         projectId: process.env.FIREBASE_PROJECT_ID,
+//         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+//     })
+    
+// });
 
 //new with firefunction
 // if (!admin.apps.length) {
