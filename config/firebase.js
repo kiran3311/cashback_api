b5a7839ceac00b5bbafd427f8d28d529e06c4943
Kiran Gangurde <89308541+kiran3311@users.noreply.github.com>
2025-11-23 18:54:18 +0530

    Update firebase.js

--- config/firebase.js
+++ config/firebase.js
@@ -1,6 +1,6 @@
 const admin = require("firebase-admin");
 
-const serviceAccount = require("../../serviceAccountKey.json"); 
+//const serviceAccount = require("../../serviceAccountKey.json"); 
 // OR use env variables in production
 
 //const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
